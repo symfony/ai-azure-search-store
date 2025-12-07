f16e6ed58d0ef8f8bb923b8bc80eb70dd4a57449
Oskar Stark <oskarstark@googlemail.com>
2025-12-07 16:09:32 +0100

    [Store][Azure] Rename `Azure` to `AzureSearch` in namespace

---
+++ SearchStore.php
@@ -0,0 +1,123 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Store\Bridge\AzureSearch;
+
+use Symfony\AI\Platform\Vector\NullVector;
+use Symfony\AI\Platform\Vector\Vector;
+use Symfony\AI\Store\Document\Metadata;
+use Symfony\AI\Store\Document\VectorDocument;
+use Symfony\AI\Store\StoreInterface;
+use Symfony\Component\Uid\Uuid;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+/**
+ * @author Christopher Hertel <mail@christopher-hertel.de>
+ */
+final class SearchStore implements StoreInterface
+{
+    /**
+     * @param string $vectorFieldName The name of the field int the index that contains the vector
+     */
+    public function __construct(
+        private readonly HttpClientInterface $httpClient,
+        private readonly string $endpointUrl,
+        #[\SensitiveParameter] private readonly string $apiKey,
+        private readonly string $indexName,
+        private readonly string $apiVersion,
+        private readonly string $vectorFieldName = 'vector',
+    ) {
+    }
+
+    public function add(VectorDocument ...$documents): void
+    {
+        $this->request('index', [
+            'value' => array_map([$this, 'convertToIndexableArray'], $documents),
+        ]);
+    }
+
+    public function query(Vector $vector, array $options = []): iterable
+    {
+        $result = $this->request('search', [
+            'vectorQueries' => [$this->buildVectorQuery($vector)],
+        ]);
+
+        foreach ($result['value'] as $item) {
+            yield $this->convertToVectorDocument($item);
+        }
+    }
+
+    /**
+     * @param array<string, mixed> $payload
+     *
+     * @return array<string, mixed>
+     */
+    private function request(string $endpoint, array $payload): array
+    {
+        $url = \sprintf('%s/indexes/%s/docs/%s', $this->endpointUrl, $this->indexName, $endpoint);
+        $result = $this->httpClient->request('POST', $url, [
+            'headers' => [
+                'api-key' => $this->apiKey,
+            ],
+            'query' => ['api-version' => $this->apiVersion],
+            'json' => $payload,
+        ]);
+
+        return $result->toArray();
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    private function convertToIndexableArray(VectorDocument $document): array
+    {
+        return array_merge([
+            'id' => $document->id,
+            $this->vectorFieldName => $document->vector->getData(),
+        ], $document->metadata->getArrayCopy());
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     */
+    private function convertToVectorDocument(array $data): VectorDocument
+    {
+        return new VectorDocument(
+            id: Uuid::fromString($data['id']),
+            vector: !\array_key_exists($this->vectorFieldName, $data) || null === $data[$this->vectorFieldName]
+                ? new NullVector()
+                : new Vector($data[$this->vectorFieldName]),
+            metadata: new Metadata($data),
+        );
+    }
+
+    /**
+     * @return array{
+     *     kind: 'vector',
+     *     vector: float[],
+     *     exhaustive: true,
+     *     fields: non-empty-string,
+     *     weight: float,
+     *     k: int,
+     * }
+     */
+    private function buildVectorQuery(Vector $vector): array
+    {
+        return [
+            'kind' => 'vector',
+            'vector' => $vector->getData(),
+            'exhaustive' => true,
+            'fields' => $this->vectorFieldName,
+            'weight' => 0.5,
+            'k' => 5,
+        ];
+    }
+}
